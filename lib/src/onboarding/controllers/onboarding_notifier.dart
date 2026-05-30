a7794896b2110393de6493dd76e7a24a15bf3387
FarishNA <farishana426@gmail.com>
2026-05-30 21:04:23 +0530

    fix: resolve merge conflicts and flutter analyze errors

--- lib/src/onboarding/controllers/onboarding_notifier.dart
+++ lib/src/onboarding/controllers/onboarding_notifier.dart
@@ -1,23 +1,5 @@
 import 'package:flutter/material.dart';
 
-<<<<<<< HEAD
-class OnboardingNotifier extends ChangeNotifier {
-
-  // Private variable
-  int _selectedPage = 0;
-
-  // Getter
-  int get selectedPage => _selectedPage;
-
-  // Setter
-  set setSelectedPage(int index) {
-    _selectedPage = index;
-    
-    // Notify UI to rebuild
-    notifyListeners();
-  }
-}
-=======
 class OnboardingNotifier with ChangeNotifier {
   int _selectedPage = 0;
 
@@ -28,4 +10,3 @@ class OnboardingNotifier with ChangeNotifier {
     notifyListeners();
   }
 }
->>>>>>> 4dd434b4efd5c81b5151152d3ad14a57285b5e97
--- lib/src/onboarding/widgets/onboarding_page_two.dart
+++ lib/src/onboarding/widgets/onboarding_page_two.dart
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import 'package:fashion_app/common/utils/kstrings.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-class OnboardingPageTwo extends StatelessWidget {
-  const OnboardingPageTwo({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-     return Container(
-      width:ScreenUtil().screenWidth,
-      height: ScreenUtil().screenHeight,
-      child:Stack(
-        children: [
-          Image.asset('R.ASSETS_IMAGES_WISHLIST_PNG',fit: BoxFit.cover,),
-          Positioned(
-            bottom: 100.h,
-            left: 20.w,
-            right: 20.w,
-            child: Text(AppText.kOnboardWishListMessage ,textAlign: TextAlign.center,style: TextStyle(fontSize: 24.sp,fontWeight: FontWeight.bold,color: const Color.fromARGB(255, 46, 23, 23)),)
-          )
-        ]
-      ),
-    );
-  }
-}
-=======
 import 'package:fashion_app/common/utils/kcolors.dart';
 import 'package:fashion_app/common/utils/kstrings.dart';
 import 'package:fashion_app/common/widgets/app_style.dart';
@@ -61,4 +33,3 @@ class OnboardingScreenTwo extends StatelessWidget {
     );
   }
 }
->>>>>>> 4dd434b4efd5c81b5151152d3ad14a57285b5e97
