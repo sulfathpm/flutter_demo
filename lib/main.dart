ed42af0e22021535887deea57b421a4858b2ae1b
sulfathpm <sulfathpm2@gmail.com>
2026-06-01 14:10:11 +0530

    Resolve stash conflict in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -25,16 +25,16 @@ void main() async {
   await GetStorage.init();
   runApp(MultiProvider(
     providers: [
-     ChangeNotifierProvider(create: (_) => OnboardingNotifier()),
-     ChangeNotifierProvider(create: (_) => TabIndexNotifier()),
-     ChangeNotifierProvider(create: (_) => CategoryNotifier()),
-     ChangeNotifierProvider(create: (_) => HomeTabNotifier()),
-     ChangeNotifierProvider(create: (_) => ProductNotifier()),
-     ChangeNotifierProvider(create: (_) => ColorSizesNotifier()),
-     ChangeNotifierProvider(create: (_) => AuthNotifier()),
-     ChangeNotifierProvider(create: (_) => PasswordNotifier()),
+      ChangeNotifierProvider(create: (_) => OnboardingNotifier()),
+      ChangeNotifierProvider(create: (_) => TabIndexNotifier()),
+      ChangeNotifierProvider(create: (_) => CategoryNotifier()),
+      ChangeNotifierProvider(create: (_) => HomeTabNotifier()),
+      ChangeNotifierProvider(create: (_) => ProductNotifier()),
+      ChangeNotifierProvider(create: (_) => ColorSizesNotifier()),
+      ChangeNotifierProvider(create: (_) => AuthNotifier()),
+      ChangeNotifierProvider(create: (_) => PasswordNotifier()),
     ],
-     child: const MyApp(),
+    child: const MyApp(),
   ));
 }
 
@@ -45,22 +45,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
     return ScreenUtilInit(
-        designSize: screenSize,
-        minTextAdapt: true,
-        splitScreenMode: false,
-        useInheritedMediaQuery: true,
-        builder: (_, child) {
-          return MaterialApp.router(
-            debugShowCheckedModeBanner: false,
-            title: AppText.kAppName,
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              useMaterial3: true,
-            ),
-            routerConfig: router,
-          );
-        },
-        child: const SplashScreen());
+      designSize: screenSize,
+      minTextAdapt: true,
+      splitScreenMode: false,
+      useInheritedMediaQuery: true,
+      builder: (_, child) {
+        return MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          title: AppText.kAppName,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          routerConfig: router,
+        );
+      },
+      child: const SplashScreen(),
+    );
   }
 }
 
